464d9b13474db93d49866ef4fb090f1c053063a7
Bart Duisters <bartduisters@bartduisters.com>
2022-11-14 20:48:48 +0100

    einde stream

--- src/pages/RefPage.vue
+++ src/pages/RefPage.vue
@@ -1,3 +1,37 @@
 <template>
-  <p>Ref Tutorial</p>
+  <p>{{ title }}</p>
+  <p>{{ persoon }}</p>
+  <p>{{ samson }}</p>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const samson = ref('Samson');
+    /*
+    {
+      value: 'Samson'
+    }
+    */
+
+    const title = ref('Ref uitleg!');
+    const persoon = ref('John Duck');
+
+    setTimeout(() => {
+      console.log('title', title.value);
+      title.value = 'Ref uitleg is veranderd!';
+      persoon.value = 'Janine Duck';
+      samson.value = 'Samson is veranderd!';
+      console.log('title', title.value);
+    }, 5000);
+
+    return {
+      title,
+      persoon,
+      samson,
+    };
+  },
+});
+</script>
